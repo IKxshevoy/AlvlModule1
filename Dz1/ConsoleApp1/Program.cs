8afc2abee5a71aa2238a3edd1a59ef0ebbd2a84a
IKxshevoy <ivan.koshevoyyy@gmail.com>
2022-04-23 00:07:06 +0300

    hw01

---
+++ Dz1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Homework 1
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_name;
+            string second_name;
+            int age;
+            Console.WriteLine("Введите Ваше имя");
+            first_name = Console.ReadLine();
+            Console.WriteLine("Введите Вашу фамилию");
+            second_name = Console.ReadLine();
+            Console.WriteLine("Введите Ваш возраст");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 100)
+            {
+                Console.WriteLine($"{first_name} {second_name}, до 100 вам осталось {100 - age} лет");
+            }
+            else if (age == 100)
+            {
+                Console.WriteLine($"{first_name} {second_name}, в данный момент вам 100 лет");
+            }
+            else
+            {
+                Console.WriteLine($"{first_name} {second_name}, вам исполнилось 100 лет, {age-100} лет назад");
+            }
+        }
+    }
+}
